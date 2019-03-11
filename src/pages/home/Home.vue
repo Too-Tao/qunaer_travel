7a6a40673bf2cc5ea08d865b671fa7882ff7b3a0
Too-Tao <599366020@qq.com>
2019-03-11 14:24:27 +0800

    完成icon部分

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -3,7 +3,7 @@
     <home-header/>
     <home-swiper/>
     <home-icons/>
-    <home-recommend/>
+    <!-- <home-recommend/> -->
   </div>
 </template>
 
@@ -11,14 +11,14 @@
 import HomeHeader from './components/Header.vue'
 import HomeSwiper from './components/Swiper.vue'
 import HomeIcons from './components/Icons.vue'
-import HomeRecommend from './components/Recommend.vue'
+// import HomeRecommend from './components/Recommend.vue'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
     HomeSwiper,
     HomeIcons,
-    HomeRecommend
+    // HomeRecommend
   }
 }
 </script>
